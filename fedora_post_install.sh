126da05ea672fcfc9ef1acf534917600a9e4bcee
1Meracle1 <bahorin.ivan@gmail.com>
2024-08-11 18:38:00 +0200

    first commit

---
+++ fedora_post_install.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# https://docs.fedoraproject.org/en-US/quick-docs
+
+set -e
+
+# install fish
+dnf install fish
+fish
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+fisher install jorgebucaran/nvm.fish
+fisher install IlanCosman/tide@v5
+chsh -s /usr/bin/fish
+
+# add flathub
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub com.sublimetext.three
+flatpak install flathub org.telegram.desktop
+flatpak install flathub com.discordapp.Discord
+flatpak install flathub com.slack.Slack
+flatpak install flathub com.github.IsmaelMartinez.teams_for_linux
+flatpak install flathub md.obsidian.Obsidian
+flatpak install flathub com.visualstudio.code
+flatpak install flathub com.spotify.Client
+
+# install base packages
+dnf install git fastfetch helix cairo swaybg waybar mako xdg-desktop-portal-gtk xdg-desktop-portal-gnome \
+  fuzzel alacritty nasm llvm clang-tools-extra 
+
+# install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+rustup component add rust-analyzer
+
+# install niri
+mkdir -p dev/external/
+cd dev/external/
+git clone https://github.com/YaLTeR/niri
+cd niri
+cargo build --release
+# add niri to sddm login manager
+cp target/release/niri 	/usr/bin/
+cp resources/niri-session 	/usr/bin/
+cp resources/niri.desktop 	/usr/share/wayland-sessions/
+cp resources/niri-portals.conf 	/usr/share/xdg-desktop-portal/
+cp resources/niri.service 	/usr/lib/systemd/user/
+cp resources/niri-shutdown.target 	/usr/lib/systemd/user/
+
+# install virt-manager
+dnf install @virtualization
+systemctl start libvirtd 
+systemctl enable libvirtd 
+usermod -a -G libvirt $(whoami)
+
+# for ssh
+set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
